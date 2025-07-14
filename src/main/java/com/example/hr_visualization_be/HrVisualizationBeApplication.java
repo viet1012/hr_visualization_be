f8aef84e658d072355a29890f1165a3824a9e715
Viet Ta <89030236+viet1012@users.noreply.github.com>
2025-07-14 10:29:53 +0700

    init project

---
+++ src/main/java/com/example/hr_visualization_be/HrVisualizationBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.hr_visualization_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrVisualizationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrVisualizationBeApplication.class, args);
+	}
+
+}
